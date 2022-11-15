e2d22278beacf77453fc7abd52fb00c80664a817
hastomi <hastomi.riduan10@gmail.com>
2022-11-15 17:17:48 +0700

    schedule, asset, button, end year challenges

---
+++ Reclean/Badges/BadgesFinishedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BadgesViewController.swift
+//  Reclean
+//
+//  Created by Hastomi Riduan Munthe on 13/11/22.
+//
+
+import UIKit
+
+class BadgesFinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
