c0add09e071ea9246098d553250ff9b68cfaebbb
hastomi <hastomi.riduan10@gmail.com>
2022-11-16 15:21:47 +0700

    navigation control, constrain first page

--- Reclean/ViewController.swift
+++ Reclean/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonStart: UIButton!
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonStarted(_ sender: Any) {
+        if let navigationController = navigationController {
+            let viewController = SignInUiViewController()
+            navigationController.pushViewController(viewController,animated:true)}
+    }
+    
 }
 
