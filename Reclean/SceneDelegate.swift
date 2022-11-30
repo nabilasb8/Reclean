d9cd8eae58f35269d2d844dd79298039b21b622e
hastomi <hastomi.riduan10@gmail.com>
2022-11-30 16:54:06 +0700

    sign in with apple

--- Reclean/SceneDelegate.swift
+++ Reclean/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let rootViewController = ScheduleViewController()
+        let rootViewController = MainViewController()
 //        let area = AreaRepository.shared.getAreas()[0]
 //        rootViewController.setArea(area: area)
         
